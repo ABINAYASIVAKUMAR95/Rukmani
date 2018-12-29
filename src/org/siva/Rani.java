663c4546b2321045d9a54060c6bde3cb1454c2b9
ABINAYASIVAKUMAR95 <sabinayakumar95@gmail.com>
2018-12-29 22:52:30 +0530

    new methods added

---
+++ src/org/siva/Rani.java
@@ -0,0 +1,19 @@
+package org.siva;
+
+public class Rani {
+	public static void Scooty() {
+		System.out.println("this is new");
+
+	}
+	public static void car() {
+		// TODO Auto-generated method stub
+		System.out.println("this is old");
+
+	}
+	public static void main(String[] args) {
+		car();
+		Scooty();
+	}
+
+
+}
